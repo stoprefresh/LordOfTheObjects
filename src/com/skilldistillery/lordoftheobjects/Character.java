0398b62fb39ebb077883ff4dc653c7accd85ef32
Miguel <marsigliamiguel@protonmail.com>
2019-05-17 19:48:22 -0600

    Player create outline

--- src/com/skilldistillery/lordoftheobjects/Character.java
+++ src/com/skilldistillery/lordoftheobjects/Character.java
@@ -1,10 +1,41 @@
 package com.skilldistillery.lordoftheobjects;
 
 public class Character {
-	private int health;
-	private int name;
-	private boolean alive;
-	private boolean reangeWeapon;
+	protected int health;
+	protected String name;
+	protected boolean alive;
+	protected boolean rangeWeapon;
+	
+	
+	
+	
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isAlive() {
+		return alive;
+	}
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+	public boolean isRangeWeapon() {
+		return rangeWeapon;
+	}
+	public void setRangeWeapon(boolean rangeWeapon) {
+		this.rangeWeapon = rangeWeapon;
+	}
+	
+//	public Character() {}
+	
 	
 	
 	
--- src/com/skilldistillery/lordoftheobjects/ShipGame.java
+++ src/com/skilldistillery/lordoftheobjects/ShipGame.java
@@ -1,5 +1,69 @@
 package com.skilldistillery.lordoftheobjects;
 
-public class ShipGame {
+import java.util.Scanner;
 
+public class ShipGame {
+	private boolean gameOver;
+	private int difficutly;
+	private boolean playerTurn;
+	private int turnCount;
+	private Player playerOne;
+	
+	
+	public ShipGame(boolean gameOver, boolean playerTurn, int turnCount) {
+		this.gameOver = false;
+		this.playerTurn = true;
+		this.turnCount = 0;
+		
+	}
+	
+	public void playerCreate(Scanner input) {
+		System.out.println("Welcome to the Game");
+		System.out.println("Please enter your name: ");
+		playerOne = new Player(input.nextLine());
+		System.out.println("Please select a difficulty level(1-3): ");
+		difficutly = input.nextInt();
+		System.out.println(playerOne.toString());
+		
+	}
+	
+
+	
+
+	public boolean isGameOver() {
+		return gameOver;
+	}
+	public void setGameOver(boolean gameOver) {
+		this.gameOver = gameOver;
+	}
+	public int getDifficutly() {
+		return difficutly;
+	}
+	public void setDifficutly(int difficutly) {
+		this.difficutly = difficutly;
+	}
+	public boolean isPlayerTurn() {
+		return playerTurn;
+	}
+	public void setPlayerTurn(boolean playerTurn) {
+		this.playerTurn = playerTurn;
+	}
+	public int getTurnCount() {
+		return turnCount;
+	}
+	public void setTurnCount(int turnCount) {
+		this.turnCount = turnCount;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
