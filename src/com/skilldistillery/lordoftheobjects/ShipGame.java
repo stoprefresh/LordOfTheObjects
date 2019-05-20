53149725ae4d86228cdcb549b620948351f4750a
Miguel <marsigliamiguel@protonmail.com>
2019-05-20 10:27:53 -0600

    Running application

--- src/com/skilldistillery/lordoftheobjects/ShipGame.java
+++ src/com/skilldistillery/lordoftheobjects/ShipGame.java
@@ -80,14 +80,15 @@ public class ShipGame {
 					engageRound(totalHostiles, count, kb, playerChoice);
 				}
 			} 
-			playerChoice = playerChoiceSafe(kb);	
+			
+				
+			if (playerOne.getScore() == gameScore) {
+				printEndGameMessage();
+				gameOver = true;
+			}
 			
 		} while (playerChoice != 2);
 
-		if (playerOne.getScore() == gameScore) {
-			printEndGameMessage();
-			gameOver = true;
-		}
 	}
 
 	// Game turn system to be restored.
@@ -154,7 +155,6 @@ public class ShipGame {
 						System.out.println("**************");
 						totalHostiles = totalHostiles - 1;
 						count++;
-						hostileRoom.getActiveHostile()[i] = null;
 					} 
 					if(totalHostiles > 0) {
 						if (characterHit(.85, playerOne.getName())) {
